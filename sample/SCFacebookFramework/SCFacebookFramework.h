56cd80a32233920c076fb250cb36becd12977d86
Son Nguyen <sonnn1992@gmail.com>
2017-03-06 23:38:01 +0700

    no message

---
+++ sample/SCFacebookFramework/SCFacebookFramework.h
@@ -0,0 +1,19 @@
+//
+//  SCFacebookFramework.h
+//  SCFacebookFramework
+//
+//  Created by Son Nguyen on 3/4/17.
+//  Copyright © 2017 SiriusCode Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SCFacebookFramework.
+FOUNDATION_EXPORT double SCFacebookFrameworkVersionNumber;
+
+//! Project version string for SCFacebookFramework.
+FOUNDATION_EXPORT const unsigned char SCFacebookFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SCFacebookFramework/PublicHeader.h>
+
+
